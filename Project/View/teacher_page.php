e5d2516d71591cc36e98274005e9c9e34f83afba
Tim Almamedov <tim.almamedov@gmail.com>
2018-07-24 15:48:09 +0200

    teacher view templaet

---
+++ Project/View/teacher_page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php include 'inc/navbar.php' ?>
+<main>
+    <div class="container-fluid mt-5">
+        <div class="row">
+            <div class="left-inner-container col-lg-9 col-md-9 col-sm-9">
+                <div class="ticket-container"></div>
+            </div>
+            <div class="right-inner-container col-lg-3 col-md-3">
+                <div class="profile-widget">
+                    <div class="profile-widget-avatar"></div>
+                    <div class="profile-widget-text">
+                        <p>Teacher X</p>
+                        <p>Number of students: 15</p>
+                        <p>Number of opened tickets: 34</p>
+                    </div>
+                </div>
+                <button class="btn btn-light mt-5 ml-5" style="border: 1px solid #7634C4;">Register a new student</button>
+            </div>
+        </div>
+    </div>
+</main>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+<script src="../js/teacher_view.js"></script>
+</body>
+</html>
---
+++ Project/js/teacher_view.js
@@ -0,0 +1,38 @@
+let items = [];
+$.getJSON("../js/dummy_data.json", (data) => {
+        $.each(data, (key, val) => {
+                items.push(val)
+            })
+for (let i = 0; i < items.length; i++) {
+    $(`.ticket-container:eq(${0})`).append(`
+    <div class="row">
+            <div class="ticket-box col-lg-6 col-md-6 col-sm-9 col-9 offset-lg-3 offset-md-3">
+                <div class="green-label"></div>
+                <div class="orange-label"></div>
+                <p class="ticket-date">Posted 45 minutes ago.</p>
+                <p class="ticket-title"><strong>Title: </strong>${items[i].title}</p>
+                <button class="expand-ticket"><i class="fas fa-angle-double-down down"></i></button>
+                <div class="panel">
+                    <p><strong>Description: </strong><br>${items[i].description}</p>
+                    <div class="row">
+                    <button class="btn btn-warning col-lg-3 offset-lg-5">Take the ticket</button>
+                    <button class="btn btn-success col-lg-3 ml-2">Close the ticket</button>
+                    </div>
+                </div>
+            </div>
+            <div class="ticket-author col-lg-1 col-md-1 col-sm-1 col-1">
+                <div class="avatar text-center ml-auto mr-auto"></div>
+                <p class="text-center">John Doe</p>
+            </div>
+        </div>
+    `)
+}
+
+var accordions = document.querySelectorAll("button.expand-ticket");
+for (var i = 0; i < accordions.length; i++) {
+accordions[i].onclick = function() {
+    this.nextElementSibling.classList.toggle("show");
+    $(this).toggleClass("rotate-button");
+};
+}
+})
