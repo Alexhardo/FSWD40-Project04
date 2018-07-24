8ec15e80052fced52e074df0d1c57647a418c1cf
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-07-24 10:46:22 +0200

    Adds logIn back-end part

--- Project/Controller/Session_controller/index_controller.php
+++ Project/Controller/Session_controller/index_controller.php
@@ -5,9 +5,13 @@
 ob_start();
 session_start();
 
+/*unset($_SESSION['user']);
+session_unset();
+session_destroy();*/
+
 //If user is already ligged in, redirect to big_list_boot.php
 if(isset($_SESSION['user'])) {
-	header('Location: big_list_boot.php');
+	header('Location: ../../index.php');
 	exit;
 }
 
@@ -17,7 +21,7 @@ if(isset($_SESSION['user'])) {
 $error = false;
 $errorMsg = '';
 
-require_once('index_model.php');
+require_once('../../Model/index_model.php');
 
 checkConnection(); //My function defined in model.php
 
@@ -41,7 +45,7 @@ if (isset($_POST['btn_login'])) { //If login button was pushed
 
 	//If user input email and pass, continue to login
 	if (!$error) {
-		$pass = hash('sha256', $pass);
+		//!!! $pass = hash('sha256', $pass);
 		//echo hash('sha256', '123456');
 		//var_dump($pass);
 
@@ -54,16 +58,16 @@ if (isset($_POST['btn_login'])) { //If login button was pushed
 				$_SESSION['user'] = $row[0]['user_id'];
 				header("Location: big_list_boot.php");
 			} else { //If passwords not match
-				var_dump($count);
+				echo 'Password is wrong!<br>';
+				/*var_dump($count);
 				var_dump($row);
-				var_dump($error);
-				echo $errorMsg;
+				var_dump($error);*/
 			}
 		} else { //If error 
-			var_dump($count);
-			var_dump($row);
-			var_dump($error);
 			echo $errorMsg;
+			/*var_dump($count);
+			var_dump($row);
+			var_dump($error);*/
 		}//else is missing because it was given in index_model.php
 	} else { //If $error == true
 		echo $errorMsg;
@@ -71,4 +75,4 @@ if (isset($_POST['btn_login'])) { //If login button was pushed
 }
 
 //Display HTML page
-require_once('index_view.php');
+require_once('../../View/index_view.php');
