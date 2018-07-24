18b94b74c274dc053566303910e4b5035038cc5e
Tim Almamedov <tim.almamedov@gmail.com>
2018-07-24 14:00:07 +0200

    changing path of assets to inside controller

--- Project/View/inc/head.php
+++ Project/View/inc/head.php
@@ -1,3 +1,4 @@
+<?php require_once('../../Config/mySQL_functions.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="../style/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="../../style/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="../../style/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../../style/style.css">
 </head>
 <body>
-    
