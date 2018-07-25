3eaf77ba9acd20f838325df9344585e1807b4544
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-07-25 17:57:50 +0200

    Locks pages for wrong users - Locks admin page for students and teachers - Locks teacher page ro students and teachers - Locks student page for admins and teachers

--- Project/Controller/Ticket_controller/teacher_ticket_controller.php
+++ Project/Controller/Ticket_controller/teacher_ticket_controller.php
@@ -1,4 +1,19 @@
 <?php 
+ob_start();
+session_start();
+
+//Check that other users not allowd to access the page
+if (!isset($_SESSION['user'])) {
+	header('Location: ../Session_controller/index_controller.php');
+	exit;
+} elseif($_SESSION['rights'] == 'admin') {
+	header('Location: ../Session_controller/admin_dashboard_controller.php');
+	exit;
+} elseif($_SESSION['rights'] == 'student') {
+	header('Location: student_ticket_controller.php');
+	exit;
+}
+
 require_once('../../Model/Tickets.php');
 
 $tickets = getTicketJson();
